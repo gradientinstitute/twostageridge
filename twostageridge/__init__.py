1babef9c6e8d6872bd920e4ef40641787c0c54de
Dan Steinberg <dan.m.steinberg@gmail.com>
2021-07-28 11:03:31 +1000

    fix linting and imports

--- twostageridge/__init__.py
+++ twostageridge/__init__.py
@@ -4,3 +4,11 @@
 
 from .estimators import TwoStageRidge, ridge_weights
 from .metrics import make_first_stage_scorer, make_combined_stage_scorer
+
+
+__all__ = (
+    'TwoStageRidge',
+    'ridge_weights',
+    'make_first_stage_scorer',
+    'make_combined_stage_scorer',
+)
